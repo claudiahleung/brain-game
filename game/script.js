51aaef1d2dec4a1dcf635d77683ec7ab4450b5d6
Diego Lopez <lopdie101@gmail.com>
2019-07-12 23:11:47 -0400

    add floors

--- game/script.js
+++ game/script.js
@@ -2,8 +2,11 @@
 const p5BrainGame = new p5(sketch => {
   const UNIT = 10;
   let gameConfig = {
-    gapDist: NaN,
-    gapWidth: NaN,
+    floorDist: 15 * UNIT,
+    gapWidth: 8 * UNIT,
+    floorHeight: 3 * UNIT,
+    floorSpeed: UNIT / 10,
+    bgColor: 220,
   };
 
   let gameState = {
@@ -12,16 +15,38 @@ const p5BrainGame = new p5(sketch => {
       y: NaN,
       radius: NaN
     },
-    gapsX: NaN,
-    gapsY: NaN,
+    floors: [],
   };
+
+  class Floor {
+    constructor() {
+      this.x = sketch.random(0, sketch.width - gameConfig.gapWidth);
+      this.y = sketch.height;
+    }
+
+    update() {
+      this.y -= gameConfig.floorSpeed;
+    }
+
+    draw() {
+      sketch.fill(0);
+      sketch.rect(0, this.y, sketch.width, gameConfig.floorHeight);
+      sketch.fill(gameConfig.bgColor);
+      sketch.rect(this.x, this.y, gameConfig.gapWidth, gameConfig.floorHeight);
+    }
+  }
+
+  sketch.reset = () => {
+    gameState.floors = [new Floor()];
+    gameState.player.x = sketch.width / 2;
+    gameState.player.y = sketch.height /2;
+  }
 
   sketch.setup = () => {
     sketch.createCanvas(300, 600);
     sketch.noStroke();
     gameState.player.radius = 2 * UNIT;
-    gameState.player.x = sketch.width / 2;
-    gameState.player.y = sketch.height /2;
+    sketch.reset();
   }
 
   sketch.draw = () => {
@@ -50,13 +75,27 @@ const p5BrainGame = new p5(sketch => {
   }
 
   sketch.update = () => {
+    gameState.floors.forEach(floor => floor.update());
+    const lastFloor = gameState.floors[gameState.floors.length - 1];
 
+    if (lastFloor.y < sketch.height - gameConfig.floorDist) {
+      gameState.floors.push(new Floor());
+    }
   };
 
   sketch.drawScene = () => {
-    sketch.background(220);
+    sketch.background(gameConfig.bgColor);
+    sketch.drawFloors();
+    sketch.drawPlayer();
+  };
+
+  sketch.drawFloors = () => {
+    gameState.floors.forEach(floor => floor.draw());
+  }
+
+  sketch.drawPlayer = () => {
     sketch.fill(0);
     sketch.ellipse(gameState.player.x, gameState.player.y,
       2 * gameState.player.radius);
-  };
+  }
 }, 'brain-game-div');
