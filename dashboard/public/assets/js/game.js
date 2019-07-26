ede99e25444715d764294611f8dab5aab8d3096c
zoe.e.mikhelzon@gmail.com <gennadymikhelzon@Gennadys-MacBook-Air.local>
2019-07-25 20:20:54 -0400

    Make background red

--- dashboard/public/assets/js/game.js
+++ dashboard/public/assets/js/game.js
@@ -6,7 +6,7 @@ const p5BrainGame = new p5(sketch => {
     gapWidth: 15 * UNIT,
     floorHeight: 3 * UNIT,
     floorSpeed: UNIT / 6,
-    bgColor: 'green',
+    bgColor: 'blue',
     horizontalSpeed: UNIT / 2,
     verticalSpeed: UNIT / 3,
   };
