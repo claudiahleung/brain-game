218df95724fd0b69987b12a092cac64b31a4e921
Michelle Wang <tomichellewang@gmail.com>
2019-08-08 22:55:30 -0400

    make stream_data.py work (Stephen)

--- offline/backend/stream_data.py
+++ offline/backend/stream_data.py
@@ -3,16 +3,18 @@ import requests
 import numpy as np
 import pandas as pd 
 
-mean_df = pd.Dataframe(columns=['Channel 1', 'Channel 2', 'Channel 7', 'Channel 8'])
+data_list = []
 
-dashboard_url = "localhost:5000/data_stream"
+dashboard_url = "localhost:3000/data_stream"
 
 def print_raw(sample):
+    global mean_df
     raw_data = sample.channels_data
-    X_ready = np.array([raw_data[0], raw_data[1], raw_data[6], raw_data[7]])
-    mean_df = mean_df.append(X_ready)
-
-    if len(mean_df) == 50:
+    X_ready = [raw_data[0], raw_data[1], raw_data[6], raw_data[7]]
+    data_list.append(X_ready)
+    
+    if len(data_list) == 50:
+        mean_df = pd.DataFrame(data=data_list, columns=['Channel 1', 'Channel 2', 'Channel 7', 'Channel 8'])
         json_data = mean_df.to_json(orient='split')
         requests.post(dashboard_url, data=json_data)
 
