8c3983898f834b8ad4555dea0c64ac161cc35289
Claudia Leung <claudiahleung@gmail.com>
2019-08-31 15:09:11 -0400

    know when stream_data.py doesn't work (cf readme), make it not stop sending data after 100

--- offline/backend/stream_data.py
+++ offline/backend/stream_data.py
@@ -3,8 +3,6 @@ import requests
 import numpy as np
 import pandas as pd 
 
-# from openbci import cyton as bci
-
 data_list = []
 
 dashboard_url = "localhost:3000/data_stream"
@@ -15,11 +13,12 @@ def print_raw(sample):
     X_ready = [raw_data[0], raw_data[1], raw_data[6], raw_data[7]]
     data_list.append(X_ready)
     
-    if len(data_list) == 100:
+    if len(data_list) >= 100:
         mean_df = pd.DataFrame(data=data_list, columns=['Channel 1', 'Channel 2', 'Channel 7', 'Channel 8'])
-        print(mean_df)
-        # json_data = mean_df.to_json(orient='split')
-        # requests.post(dashboard_url, data=json_data)
+        # print(mean_df)
+        json_data = mean_df.to_json(orient='split')
+        requests.post(dashboard_url, data=json_data)
+        data_list.clear()
 
 def printData(sample):
     # os.system('clear')
@@ -30,11 +29,9 @@ def printData(sample):
         print(sample.aux_data)
     print("----------------")
 
-# board = OpenBCICyton(port=None, daisy=False)
 if __name__== "__main__": 
     port = "/dev/tty.usbserial-DM00QA1Z"
     board = OpenBCICyton(port=port, daisy=False)
     print("Board Instantiated")
-#     instance = board.start_stream(print_raw)
-#     board.start_stream(print_raw)
-    board.start_stream(printData)
+    instance = board.start_stream(print_raw)
+#     board.start_stream(printData)
